56f9d52c8bb23bbc9a1c680e23af39cbf5a7a31c
kevinkoscica <kevinkoscica@gmail.com>
2025-04-10 11:59:42 -0400

    Initial Commit

---
+++ StepCompetition/StepCompetitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepCompetitionApp.swift
+//  StepCompetition
+//
+//  Created by Kevin Koscica on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct StepCompetitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
